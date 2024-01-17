ab33014b161034cc5f4fa1317b94700924ffeb6b
Batuhan Balkilic <batuhanbalkilic@hotmail.com>
2024-01-17 12:00:26 -0500

    Added

--- src/week5_morning/NestedLoopsExercise.java
+++ src/week5_morning/NestedLoopsExercise.java
@@ -2,6 +2,16 @@ package week5_morning;
 
 public class NestedLoopsExercise {
     public static void main(String[] args) {
-        
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Lap " + i + " Finished.");
+
+            for (int j = 1; j <=3; j++) {
+
+                System.out.println("........Push up " + j + " is done");
+
+            }
+        }
+
     }
 }
